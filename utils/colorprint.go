266fe03eedd090c88ce7ca9dab0a487751185f4f
WAY29 <1162410187@qq.com>
2022-02-21 15:32:22 +0800

    ✨ feat(colorprint): add message and question

--- utils/colorprint.go
+++ utils/colorprint.go
@@ -13,6 +13,20 @@ func SuccessF(message string, args ...interface{}) {
 	fmt.Println(Cyan("[+]"), fmt.Sprintf(message, args...))
 }
 
+func Message(message string) {
+	fmt.Println(Gray(8, "[#]"), message)
+}
+func MessageF(message string, args ...interface{}) {
+	fmt.Println(Gray(8, "[#]"), fmt.Sprintf(message, args...))
+}
+
+func Question(message string) {
+	fmt.Print(Yellow("[?]"), message)
+}
+func QuestionF(message string, args ...interface{}) {
+	fmt.Print(Yellow("[?]"), fmt.Sprintf(message, args...))
+}
+
 func Failure(message string) {
 	fmt.Println(Red("[-]"), message)
 }

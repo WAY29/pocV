f059c83a0ab966af40bd86edb94371a258608f7f
WAY29 <1162410187@qq.com>
2022-02-22 13:03:49 +0800

    🦄 refactor(check):

---
+++ internal/common/check/nuclei.go
@@ -0,0 +1,36 @@
+package check
+
+import (
+	"github.com/WAY29/pocV/internal/common/errors"
+	nuclei_structs "github.com/WAY29/pocV/pkg/nuclei/structs"
+	"github.com/WAY29/pocV/utils"
+	"github.com/projectdiscovery/nuclei/v2/pkg/output"
+)
+
+func executeNucleiPoc(target string, poc *nuclei_structs.Poc) (results []*output.ResultEvent, isVul bool, err error) {
+	isVul = false
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.Wrapf(r.(error), "Run Nuclei Poc[%s] error", poc.ID)
+			isVul = false
+		}
+	}()
+
+	utils.DebugF("Run Nuclei Poc %s[%s]", target, poc.Info.Name)
+
+	e := poc.Executer
+	results = make([]*output.ResultEvent, 0, e.Requests())
+
+	err = e.ExecuteWithResults(target, func(result *output.InternalWrappedEvent) {
+		if len(result.Results) > 0 {
+			isVul = true
+		}
+		results = append(results, result.Results...)
+	})
+
+	if len(results) == 0 {
+		results = append(results, &output.ResultEvent{TemplateID: poc.ID, Matched: target})
+	}
+	return results, isVul, err
+}

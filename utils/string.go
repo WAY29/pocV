a7916ae568f736f870a5e84f6410f2a0c076e01c
WAY29 <1162410187@qq.com>
2022-02-23 09:30:34 +0800

    ✨ feat(utils.string): add ReverseString

--- utils/string.go
+++ utils/string.go
@@ -48,3 +48,13 @@ func MD5(str string) string {
 	bytes := c.Sum(nil)
 	return hex.EncodeToString(bytes)
 }
+
+//反向string
+
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
